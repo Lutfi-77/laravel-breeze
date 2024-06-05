fdbdf5ad6b5f18d10aa9f4e1e539e833cda53f33
Lutfi-77 <xenonoffic@gmail.com>
2024-06-06 06:16:40 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified', 'roleCheck'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth', 'roleCheck:customer'])->group(function () {
+    Route::get('/customer', function () {
+        return view('customer');
+    });
+
+    Route::get('/worker', function () {
+        return view('worker');
+    });
+});
+require __DIR__.'/auth.php';
